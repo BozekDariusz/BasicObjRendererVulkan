554b9a6676ec55955c2d59d42abc864a0f12f8ee
BozekDariusz <dracops66@gmail.com>
2020-12-14 22:03:05 +0100

    modified:   CW2/Source.cpp 	modified:   CW2/x64/Debug/CW2.log 	modified:   CW2/x64/Debug/CW2.tlog/CL.command.1.tlog 	modified:   CW2/x64/Debug/CW2.tlog/CL.read.1.tlog 	modified:   CW2/x64/Debug/CW2.tlog/CL.write.1.tlog 	modified:   CW2/x64/Debug/CW2.tlog/CW2.lastbuildstate 	modified:   CW2/x64/Debug/CW2.tlog/link.command.1.tlog 	modified:   CW2/x64/Debug/CW2.tlog/link.read.1.tlog 	modified:   CW2/x64/Debug/CW2.tlog/link.write.1.tlog 	modified:   CW2/x64/Debug/Source.obj 	modified:   CW2/x64/Debug/vc142.idb 	modified:   CW2/x64/Debug/vc142.pdb 	modified:   x64/Debug/CW2.exe 	modified:   x64/Debug/CW2.ilk 	modified:   x64/Debug/CW2.pdb

--- CW2/Source.cpp
+++ CW2/Source.cpp
@@ -1797,7 +1797,10 @@ private: GLFWwindow* window;
     }
 
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
-        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+       
+
+        if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) 
+            std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
 
         return VK_FALSE;
     }
